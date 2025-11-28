c9c1890405cffb2fc83fbf98febdfb2b4eaf0ed7
Salim Mwarika <sjmwatsefu@gmail.com>
2025-11-28 11:32:14 +0300

    add new vite url

--- yadi-wallets-web/src/api/axios.ts
+++ yadi-wallets-web/src/api/axios.ts
@@ -1,14 +1,13 @@
 import axios from 'axios';
 
-
 const api = axios.create({
-    baseURL: '', 
-    withCredentials: true,
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    xsrfCookieName: 'csrftoken',
-    xsrfHeaderName: 'X-CSRFToken',
+  baseURL: import.meta.env.VITE_API_URL, 
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  xsrfCookieName: 'csrftoken',
+  xsrfHeaderName: 'X-CSRFToken',
 });
 
 export default api;
