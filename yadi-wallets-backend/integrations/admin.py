e16531e9da4bc87ac4252122387f6efc8d677287
Salim Mwarika <sjmwatsefu@gmail.com>
2025-11-29 18:16:13 +0300

    Add service client

--- yadi-wallets-backend/integrations/admin.py
+++ yadi-wallets-backend/integrations/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import ServiceClient
 
-# Register your models here.
+@admin.register(ServiceClient)
+class ServiceClientAdmin(admin.ModelAdmin):
+    # This ensures you can see the key in the list view
+    list_display = ('name', 'api_key', 'is_active', 'created_at')
+    
+    # This allows you to see the key inside the edit page (as read-only)
+    readonly_fields = ('api_key',)
+
+    # Use this to search if you have many clients
+    search_fields = ('name', 'api_key')
